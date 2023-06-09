184ab070940636b8f018b35092a316c3a22ac399
Agatha A Msola <agatha@aims.ac.tz>
2023-06-09 14:14:20 +0200

    images and words containers created

---
+++ main.js
@@ -0,0 +1,9 @@
+let pic1 = document.getElementById("picture1");
+let pic2 = document.getElementById("picture2");
+let pic3 = document.getElementById("picture3");
+
+setTimeout(function () {
+  pic1.style.visibility = "hidden";
+  pic2.style.visibility = "hidden";
+  pic3.style.visibility = "hidden";
+}, 3000);
