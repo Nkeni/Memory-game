fe061c57dbb436355548d06498d7ccdae220f0de
Agatha A Msola <agatha@aims.ac.tz>
2023-06-11 16:45:44 +0200

    images added

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 let pic1 = document.getElementById("picture1");
 let pic2 = document.getElementById("picture2");
 let pic3 = document.getElementById("picture3");
+let pic4 = document.getElementById("picture4");
+let pic5 = document.getElementById("picture5");
+let pic6 = document.getElementById("picture6");
 
 setTimeout(function () {
   pic1.style.visibility = "hidden";
   pic2.style.visibility = "hidden";
   pic3.style.visibility = "hidden";
+  pic4.style.visibility = "hidden";
+  pic5.style.visibility = "hidden";
+  pic6.style.visibility = "hidden";
 }, 1000);
 
 const word = document.querySelectorAll(".word");
@@ -55,33 +61,74 @@ function drop(e) {
   console.log("dropped target;", e.target.id);
   if (draggable.id == e.target.id) {
     e.target.classList.add("drag-over-correct");
-    alert("Correct");
   } else {
     e.target.classList.add("drag-over-wrong");
-    alert("Wrong,YOU LOST, try again");
+  }
+  //! condition to check if all matches are correct
+
+  if (
+    pictureContainer[0].classList.value.includes("drag-over-correct") &&
+    pictureContainer[1].classList.value.includes("drag-over-correct") &&
+    pictureContainer[2].classList.value.includes("drag-over-correct") &&
+    pictureContainer[3].classList.value.includes("drag-over-correct") &&
+    pictureContainer[4].classList.value.includes("drag-over-correct") &&
+    pictureContainer[5].classList.value.includes("drag-over-correct")
+  ) {
+    alert("Congratulations, You have WON!!!");
+    pictureContainer[0].classList.add("drag-over-win");
+    pictureContainer[1].classList.add("drag-over-win");
+    pictureContainer[2].classList.add("drag-over-win");
+    pictureContainer[3].classList.add("drag-over-win");
+    pictureContainer[4].classList.add("drag-over-win");
+    pictureContainer[5].classList.add("drag-over-win");
+
+    //! remove the win sign after 1 second
+    setTimeout(function () {
+      pictureContainer[0].classList.remove("drag-over-win");
+      pictureContainer[1].classList.remove("drag-over-win");
+      pictureContainer[2].classList.remove("drag-over-win");
+      pictureContainer[3].classList.remove("drag-over-win");
+      pictureContainer[4].classList.remove("drag-over-win");
+      pictureContainer[5].classList.remove("drag-over-win");
+      pictureContainer[0].classList.remove("drag-over-correct");
+      pictureContainer[1].classList.remove("drag-over-correct");
+      pictureContainer[2].classList.remove("drag-over-correct");
+      pictureContainer[3].classList.remove("drag-over-correct");
+      pictureContainer[4].classList.remove("drag-over-correct");
+      pictureContainer[5].classList.remove("drag-over-correct");
+    }, 1000);
+  } else if (
+    pictureContainer[0].classList.value.includes("drag-over-wrong") ||
+    pictureContainer[1].classList.value.includes("drag-over-wrong") ||
+    pictureContainer[2].classList.value.includes("drag-over-wrong") ||
+    pictureContainer[3].classList.value.includes("drag-over-wrong") ||
+    pictureContainer[4].classList.value.includes("drag-over-wrong") ||
+    pictureContainer[5].classList.value.includes("drag-over-wrong")
+  ) {
+    e.target.classList.add("drag-over-wrong");
+    alert("Sorry, You have LOST...try again!!!");
+
     //! After loosing, show images for 1 second again
     pic1.style.visibility = "visible";
     pic2.style.visibility = "visible";
     pic3.style.visibility = "visible";
+    pic4.style.visibility = "visible";
+    pic5.style.visibility = "visible";
+    pic6.style.visibility = "visible";
 
     //! Hide images after one second again
     setTimeout(function () {
       pic1.style.visibility = "hidden";
       pic2.style.visibility = "hidden";
       pic3.style.visibility = "hidden";
+      pic4.style.visibility = "hidden";
+      pic5.style.visibility = "hidden";
+      pic6.style.visibility = "hidden";
     }, 1000);
     //! remove the wrong sign after 2 second
     setTimeout(function () {
       e.target.classList.remove("drag-over-wrong");
     }, 2000);
-  }
-  //! condition to check if all matches are correct
-  if (
-    pictureContainer[0].classList.value.includes("drag-over-correct") &&
-    pictureContainer[1].classList.value.includes("drag-over-correct") &&
-    pictureContainer[2].classList.value.includes("drag-over-correct")
-  ) {
-    alert("Congratulations, You have won!!!");
   }
 
   // display the draggable element
